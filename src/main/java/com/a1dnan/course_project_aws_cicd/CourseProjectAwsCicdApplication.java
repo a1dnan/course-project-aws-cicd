20648d407081aa2b8f1caf2b62e149d3a22bc870
a1dnan <aaliladnane@gmail.com>
2025-06-04 16:39:47 +0100

    add project files

---
+++ src/main/java/com/a1dnan/course_project_aws_cicd/CourseProjectAwsCicdApplication.java
@@ -0,0 +1,13 @@
+package com.a1dnan.course_project_aws_cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseProjectAwsCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseProjectAwsCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/a1dnan/course_project_aws_cicd/CourseProjectAwsCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a1dnan.course_project_aws_cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseProjectAwsCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
